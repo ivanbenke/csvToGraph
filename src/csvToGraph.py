fa6f28fd3426ffe610f7b4b328bc3432fe618541
ivanbenke <benke.ivan@gmail.com>
2019-10-25 18:32:54 +0200

    Remove complicated code, add CheckButton functionality

--- src/csvToGraph.py
+++ src/csvToGraph.py
@@ -6,70 +6,71 @@ import os
 root = Tk()
 
 main_frame = Frame(root)
-
 browse_frame = Frame(main_frame)
-
 info_frame = Frame(main_frame)
 
+checked_dict = {}
+
 for frame in [main_frame, browse_frame, info_frame]:
-	frame.pack(expand = True, fill = "both", side = "left")
+    frame.pack(expand = True, fill = "both", side = "left")
 
 def apply_settings():
-	dir_name = askdirectory()
-	print(dir_name)
+    for (key, value) in checked_dict.items():
+        if value.get() == 1:
+            print(key)
+            print(value.get())
+    dir_name = askdirectory()
+    print(dir_name)
 
 def open_csv_file():
-	for widget in info_frame.winfo_children():
-		widget.destroy()
-	
-	file_name = askopenfilename(initialdir = root, title = "Select CSV file", filetypes = (("CSV files", "*.csv"),("all files", "*.*")))
+    for widget in info_frame.winfo_children():
+        widget.destroy()
 
-	if file_name:
-		header_array = []
-		row_number = 0
-		col_number = 0
-		num_of_rows = 0
+    file_name = askopenfilename(initialdir = root, title = "Select CSV file", filetypes = (("CSV files", "*.csv"),("all files", "*.*")))
 
-		header_window = Toplevel(root)
-		header_frame = Frame(header_window)
-		apply_frame = Frame(header_window)
-		for frame in [header_frame, apply_frame]:
-			frame.pack(expand = True, fill = "both", side = "left")
+    if file_name:
+        header_array = []
+        row_number = 0
+        col_number = 0
+        num_of_rows = 0
 
-		apply_button = Button(apply_frame, text = "Apply", command = apply_settings)
-		apply_button.pack(side = "bottom")
+        header_window = Toplevel(root)
+        header_frame = Frame(header_window)
+        apply_frame = Frame(header_window)
+        for frame in [header_frame, apply_frame]:
+            frame.pack(expand = True, fill = "both", side = "left")
 
-		with open(file_name) as csv_file:
-			data_logger = csv.reader(csv_file, delimiter=",")
-			col_number = len(next(data_logger))
-			csv_file.seek(0)
+        apply_button = Button(apply_frame, text = "Apply", command = apply_settings)
+        apply_button.pack(side = "bottom")
 
-			for row in data_logger:
-				if row_number == 0:
-					j = 1
-					for i in range(0, col_number, 1):
-						if j <= col_number:
-							header_array.append(str(row[i:j]))
-							j += 1
-					row_number += 1
-				else:
-					row_number += 1
-			num_of_rows = row_number
-			row_number = 0
-			csv_file.seek(0)
-		
-		number_row = 0
-		number_column = -1
-		for i in range(0, len(header_array), 1):
-			header_array[i] = header_array[i][2:-2]			
-			if i % 20 == 0:
-				number_row = 0
-				number_column += 1
-			Checkbutton(header_frame, text = header_array[i]).grid(row = number_row, column = number_column)
-			number_row += 1
-	else:
-		info_label = Label(info_frame, text = "File not selected")
-		info_label.pack(side = "left")
+        with open(file_name) as csv_file:
+            data_logger = csv.reader(csv_file, delimiter=",")
+            col_number = len(next(data_logger)) - 1
+            csv_file.seek(0)
+
+            for row in data_logger:
+                if row_number == 0:
+                    for element in row:
+                        if element:
+                            header_array.append(element)
+                row_number += 1
+            num_of_rows = row_number
+            row_number = 0
+            csv_file.seek(0)
+
+        number_row = 0
+        number_column = 0
+        for item in header_array:
+            checked_var = IntVar()
+            if number_row == 20:
+                number_row = 0
+                number_column += 1
+            Checkbutton(header_frame, text = item, variable = checked_var).grid(row = number_row, column = number_column)
+            checked_dict.update({item:checked_var})
+            number_row += 1
+    else:
+        info_label = Label(info_frame, text = "File not selected")
+        info_label.pack(side = "left")
 
 browse_button = Button(browse_frame, text = "Browse", command = open_csv_file)
 browse_button.pack(side = "left")
