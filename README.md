6dc1df8e76d391081eb880906220192619c3646c
ivanbenke <benke.ivan@gmail.com>
2019-10-26 11:33:58 +0200

    Add graph plotting for multiple choices

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # csvToGraph
+
+### Requirements:
+- application has to be standalone
+- load csv file
+- present the user with headers loaded from csv file
+- enable user to choose which data he wants to see
+- plot the data and save it in .svg format in desired location
--- src/csvToGraph.py
+++ src/csvToGraph.py
@@ -1,6 +1,6 @@
 import csv
 from tkinter import *
-from tkinter.filedialog import *
+from tkinter import filedialog as fd
 from matplotlib import pyplot as plt
 import os
 from copy import deepcopy
@@ -13,14 +13,13 @@ info_frame = Frame(main_frame)
 
 header_array = []
 checked_dict = {}
-global g_data_logger
 
 for frame in [main_frame, browse_frame, info_frame]:
     frame.pack(expand = True, fill = "both", side = "left")
 
 def apply_settings(f_data_logger, f_date_index, f_time_index):
     index_array = []
-    dir_name = askdirectory()
+    dir_name = fd.askdirectory()
     print(dir_name)
     if dir_name:
         for (key, value) in checked_dict.items():
@@ -29,7 +28,7 @@ def apply_settings(f_data_logger, f_date_index, f_time_index):
                 # print(value.get())
                 for element in header_array:
                     if key == element:
-                        index_array.append(header_array.index(element) + 2) # 2 is for Date and Time which are in .csv file but not in header_array
+                        index_array.append(header_array.index(element))
         for index in index_array:
             values_array = []
             time_array = []
@@ -37,15 +36,14 @@ def apply_settings(f_data_logger, f_date_index, f_time_index):
                 values_array.append(float(row[index]))
                 time_array.append(row[f_time_index])
             plt.plot(time_array, values_array)
-            plt.xlabel("Time")
-            plt.ylabel("y axis")
-            graph_filename = dir_name + "/graph.png"
-            plt.savefig(graph_filename)#, bbox_inches="tight")
+            plt.xlabel(header_array[f_time_index])
+            plt.ylabel(header_array[index])
+            graph_filename = dir_name + "/" + header_array[index]
+            plt.savefig(graph_filename, bbox_inches="tight")
+            plt.clf()
         print(index_array)
         # plot graph
         # print(graph_filename)
-        # with open(graph_filename, "w+") as graph_file:
-        #     pass
     else:
         print("Please select an output directory")
 
@@ -53,7 +51,7 @@ def open_csv_file():
     for widget in info_frame.winfo_children():
         widget.destroy()
 
-    csv_filename = askopenfilename(initialdir = root, title = "Select CSV file", filetypes = (("CSV files", "*.csv"), ("all files", "*.*")))
+    csv_filename = fd.askopenfilename(initialdir = root, title = "Select CSV file", filetypes = (("CSV files", "*.csv"), ("all files", "*.*")))
 
     if csv_filename:
         row_number = 0
@@ -67,6 +65,7 @@ def open_csv_file():
 
         # apply_button = Button(apply_frame, text = "Apply", command = lambda: apply_settings(data_logger))
         # apply_button.pack(side = "bottom")
+        g_data_logger = []
 
         with open(csv_filename) as csv_file:
             data_logger = csv.reader(csv_file, delimiter = ",")
@@ -80,7 +79,7 @@ def open_csv_file():
                 if row_number == 0:
                     for element in row:
                         # print(element)
-                        if element and element != "Date" and element != "Time":
+                        if element:# and element != "Date" and element != "Time":
                             header_array.append(element)
                         if element == "Date":
                             date_index = row.index(element)
@@ -99,18 +98,25 @@ def open_csv_file():
         number_row = 0
         number_column = 0
         for item in header_array:
-            checked_var = IntVar()
-            if number_row == 20:
-                number_row = 0
-                number_column += 1
-            Checkbutton(header_frame, text = item, variable = checked_var).grid(row = number_row, column = number_column)
-            checked_dict.update({item:checked_var})
-            number_row += 1
+            if item != "Date" and item != "Time":
+                checked_var = IntVar()
+                if number_row == 20:
+                    number_row = 0
+                    number_column += 1
+                Checkbutton(header_frame, text = item, variable = checked_var).grid(row = number_row, column = number_column)
+                checked_dict.update({item:checked_var})
+                number_row += 1
     else:
         info_label = Label(info_frame, text = "File not selected")
         info_label.pack(side = "left")
 
 browse_button = Button(browse_frame, text = "Browse", command = open_csv_file)
 browse_button.pack(side = "left")
+
+def on_closing():
+    plt.close()
+    root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
